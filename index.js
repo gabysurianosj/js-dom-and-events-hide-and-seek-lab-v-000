5d62a2e7bd9eb88a4a13a9a9c8124f990dcadb9e
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 16:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   const list = document.querySelectorAll('ul.ranked-list li')
 
-  for (let i=0)
+  for (let i=0; i < list.length; i++){
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
+  }
+};
